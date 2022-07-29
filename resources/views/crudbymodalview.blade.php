e44261b27ee69370386eae49874b7e54b89a2f7e
AbubakrChan <89600478+AbubakrChan@users.noreply.github.com>
2022-07-29 11:46:31 +0500

    Edit button was not smooth back then.

--- resources/views/crudbymodalview.blade.php
+++ resources/views/crudbymodalview.blade.php
@@ -1,7 +1,7 @@
 {{--<script>--}}
 {{--    @include(jquery.bootstrap-growl.min.js)--}}
 {{--</script>--}}
-<!DOCTYPE html>
+    <!DOCTYPE html>
 <html lang="en">
 <head>
     <title></title>
@@ -28,9 +28,9 @@
     <h2>Data</h2>
     <hr style="{width: 300px;margin-left: auto;margin-right: auto;height: 100px;background-color:#666;opcaity: 0.5;}" ></hr>
 
-<?php
-$data = \App\mymodel::all();
-?>
+    <?php
+    $data = \App\mymodel::all();
+    ?>
 
     <table >
         <tr>
@@ -43,13 +43,13 @@ $data = \App\mymodel::all();
                 </td>
                 <td>
                     <div class="idbu">
-                    {{ $daata['name']}}
-</div>
-</td>
+                        {{ $daata['name']}}
+                    </div>
+                </td>
                 <td>
                     <div class="idbu">
 
-                    {{   $daata['email']}}
+                        {{   $daata['email']}}
                     </div>
 
                 </td>
@@ -59,7 +59,7 @@ $data = \App\mymodel::all();
                     <meta name="csrf-token" content="{{ csrf_token() }}">
                     <form  method="POST" id="abcd{{$daata['id']}}" class="idbu">
                         @csrf
-                        <button type="button" value="delete"  id="delete" name="delete"  onclick="confirmDeleteModal({{$daata['id']}})" >Delete</button>
+                        <button type="button"  value="delete"  id="delete" class="btn btn-default"  name="delete"  onclick="confirmDeleteModal({{$daata['id']}})" >Delete</button>
 
                     </form>
                 </td>
@@ -67,52 +67,46 @@ $data = \App\mymodel::all();
                 <td>
 
                     <meta name="csrf-token" content="{{ csrf_token() }}">
-                    <form  method="POST" id="abcde{{$daata['id']}}" >
+                    <form  method="POST" >
                         @csrf
-                    <button type="button"  data-toggle="modal"   data-target="#myModal{{$daata->id}}">Edit</button>
-                    <!--  <button type="button" value="edit"  id="edit" name="edit"  >Edit</button>-->
-                      </form>
-                      <!--<input type="submit" value="Edit"  id="edit" name="edit"  />-->
+                        <button type="button"  data-toggle="modal" class="btn btn-default"  data-target="#myModal{{$daata->id}}">Edit</button>
+                        <!--  <button type="button" value="edit"  id="edit" name="edit"  >Edit</button>-->
+                    </form>
+                    <!--<input type="submit" value="Edit"  id="edit" name="edit"  />-->
                 </td>
 
             </tr>
-        <script type="text/javascript">
-
+            <script type="text/javascript">
                 function confirmDeleteModal(id){
                     $('#deleteModal').modal();
                     $('#deleteButton').html('<a class="btn btn-danger" onclick="deleteData('+id+')">Delete</a>');
-
                 }
                 function deleteData(id){
-                        $.ajax({
-                            url: "/myajax1",
-                            type: 'POST',
-                            data:{
-                                "_token": "{{ csrf_token() }}",
-                                id:id
-                            },
-                            success: function(dataResult) {
-                                $('#deleteModal').modal('hide');
-                                if( window.localStorage ) {
-                                    if (!localStorage.getItem('firstLoad')) {
-                                        localStorage['firstLoad'] = true;
-                                        window.location.reload();
-                                    } else
-                                        localStorage.removeItem('firstLoad');
-                                    if (!localStorage.getItem('firstLoad')) {
-                                        localStorage['firstLoad'] = true;
-                                        window.location.reload();
-                                    }
-                                    //  window.location.reload();
-
+                    $.ajax({
+                        url: "/myajax1",
+                        type: 'POST',
+                        data:{
+                            "_token": "{{ csrf_token() }}",
+                            id:id
+                        },
+                        success: function(dataResult) {
+                            $('#deleteModal').modal('hide');
+                            if( window.localStorage ) {
+                                if (!localStorage.getItem('firstLoad')) {
+                                    localStorage['firstLoad'] = true;
+                                    window.location.reload();
+                                } else
+                                    localStorage.removeItem('firstLoad');
+                                if (!localStorage.getItem('firstLoad')) {
+                                    localStorage['firstLoad'] = true;
+                                    window.location.reload();
                                 }
+                                //  window.location.reload();
                             }
-                        });
-
-
-
+                        }
+                    });
                 }
-        </script>
+            </script>
 
 
 
@@ -127,15 +121,15 @@ $data = \App\mymodel::all();
                             <form  method="post" >
                                 @csrf
                                 <h3>Username.</h3>
-                                <input type="text" id="name1" name="name"  value="{{$daata->name}}" required><br>
+                                <input type="text" id="name1" name="name"  value="{{$daata->name}}"><br>
                                 <h3>Email</h3>
-                                <input type="text" id="email1" name="email" value="{{$daata->email}}" required><br><br>
+                                <input type="text" id="email1" name="email" value="{{$daata->email}}" ><br><br>
                                 <h3>Password</h3>
-                                <input type="text" id="password1" name="password"  value="{{$daata->password}}" required><br><br>
+                                <input type="text" id="password1" name="password"  value="{{$daata->password}}" ><br><br>
                             </form>
                         </div>
                         <div class="modal-footer">
-                            <form  method="post" id="abcde">
+                            <form  method="post" id="abcde{{$daata['id']}}">
 
                                 <input type="submit" class="btn btn-default" value="Update"  id="submit1" name="submit1"  />
 
@@ -146,50 +140,43 @@ $data = \App\mymodel::all();
                     </div>
                 </div>
             </div>
-        <script type="text/javascript">
+            <script type="text/javascript">
+              //  $('#abcde{{$daata['id']}}').on('click',function(event){
+                   // event.preventDefault()
+                    $('#abcde{{$daata['id']}}').on('submit',function(event){
+                        event.preventDefault()
+                        id= {{$daata->id}};
+                      //  alert   (id);
 
-            $('#abcde{{$daata['id']}}').on('click',function(event){
-                event.preventDefault()
-
-                $('#abcde').on('submit',function(event){
-                    event.preventDefault()
-                    id= {{$daata->id}};
-                    name = $('#name1').val();
-                    email = $('#email1').val();
-                    password = $('#password1').val();
-                    $.ajax({
-                    url: "/myajax2",
-                    type: 'POST', // replaced from put
-                    data:{
-                    "_token": "{{ csrf_token() }}",
-                        id:id,
-                        name:name,
-                        email:email,
-                        password:password,
-                },
-                success: function(dataResult) {
-
-                    if( window.localStorage ) {
-                        if (!localStorage.getItem('firstLoad')) {
-                            localStorage['firstLoad'] = true;
-                            window.location.reload();
-                        } else
-                            localStorage.removeItem('firstLoad');
-                    }
-
-                 //   $('#edit').show();
-                    // $(".success").show();
-
-
-
-
-
-                }
-
-                });
-                });
-            });
-        </script>
+                        name = $('#name1').val();
+                        email = $('#email1').val();
+                        password = $('#password1').val();
+                        $.ajax({
+                            url: "/myajax2",
+                            type: 'POST', // replaced from put
+                            data:{
+                                "_token": "{{ csrf_token() }}",
+                                id:id,
+                                name:name,
+                                email:email,
+                                password:password,
+                            },
+                            success: function(dataResult) {
+                                    if( window.localStorage ) {
+                                        if (!localStorage.getItem('firstLoad')) {
+                                            localStorage['firstLoad'] = true;
+                                            window.location.reload();
+                                        } else
+                                            localStorage.removeItem('firstLoad');
+                                    }
+                                //     //   $('#edit').show();
+                                //     // $(".success").show();
+                                // }
+                            }
+                        });
+                    });
+             //   });
+            </script>
 
         @endforeach
 
@@ -229,8 +216,8 @@ $data = \App\mymodel::all();
                 <div class="modal-footer">
                     <form  method="post" id="abc">
 
-                    <input type="submit"  class="btn btn-default" value="submit"  id="submit" name="submit"  />
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                        <input type="submit"  class="btn btn-default" value="submit"  id="submit" name="submit"  />
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
                     </form>
 
                 </div>
@@ -243,55 +230,50 @@ $data = \App\mymodel::all();
 
 
 <!--////////////////////////////////////////////////Insert data Script start//////////////////////////////////////////////////////////////////////////-->
-                    <script type="text/javascript">
-                    $('#abc').on('submit',function(event){
-                       event.preventDefault();
-                        name = $('#name').val();
-                        email = $('#email').val();
-                        password = $('#password').val();
-
-                        $.ajax({
-                            url: "/myajax",
-                            type:"POST",
-                            data:{
-                              "_token": "{{ csrf_token() }}",
-                                name:name,
-                                email:email,
-                                password:password,
-                            },
-                            success: function(){
-
-                                if( window.localStorage ) {
-                                    if (!localStorage.getItem('firstLoad')) {
-                                        localStorage['firstLoad'] = true;
-                                        window.location.reload();
-                                    } else
-                                        localStorage.removeItem('firstLoad');
-                                        window.location.reload();
-
-                                }
-                            },
-                            error: function (error){
-                                console.log(error.responseJSON.errors);
-                                $('#nameerror').text(error.responseJSON.errors.email);
-                                $('#emailerror').text(error.responseJSON.errors.name);
-                                $('#passworderror').text(error.responseJSON.errors.password);
-
-                            }
-                        });
-                    });
-
-                    // function clear(){
-                    //     $('#name').val('');
-                    //     $('#email').val('');
-                    //     $('#password').val('');
-                    //     $('#nameerror').val('');
-                    //     $('#emailerrorerror').val('');
-                    //     $('#passworderror').val('');
-                    //
-                    //
-                    // }
-                </script>
+<script type="text/javascript">
+    $('#abc').on('submit',function(event){
+        event.preventDefault();
+        name = $('#name').val();
+        email = $('#email').val();
+        password = $('#password').val();
+        $.ajax({
+            url: "/myajax",
+            type:"POST",
+            data:{
+                "_token": "{{ csrf_token() }}",
+                name:name,
+                email:email,
+                password:password,
+            },
+            success: function(){
+                if( window.localStorage ) {
+                    if (!localStorage.getItem('firstLoad')) {
+                        localStorage['firstLoad'] = true;
+                        window.location.reload();
+                    } else
+                        localStorage.removeItem('firstLoad');
+                    window.location.reload();
+                }
+            },
+            error: function (error){
+                console.log(error.responseJSON.errors);
+                $('#nameerror').text(error.responseJSON.errors.email);
+                $('#emailerror').text(error.responseJSON.errors.name);
+                $('#passworderror').text(error.responseJSON.errors.password);
+            }
+        });
+    });
+    // function clear(){
+    //     $('#name').val('');
+    //     $('#email').val('');
+    //     $('#password').val('');
+    //     $('#nameerror').val('');
+    //     $('#emailerrorerror').val('');
+    //     $('#passworderror').val('');
+    //
+    //
+    // }
+</script>
 <!--////////////////////////////////////////////////Insert data Script end//////////////////////////////////////////////////////////////////////////-->
 
 
@@ -299,7 +281,6 @@ $data = \App\mymodel::all();
 
 <!--//////////////////////////////////////////////Modal1 end///////////////////////////////////////////////////////////////////////////-->
 <script type="text/javascript">
-
 </script>
 <div class="container" style="padding:150px;">
 
@@ -325,7 +306,7 @@ $data = \App\mymodel::all();
     </div>
     <!--Modal ends here--->
 
-{{--    <button type="button" class="btn btn-info btn-lg" onclick="confirmDeleteModal('112')">Delete Data With Id 112</button><br>--}}
+    {{--    <button type="button" class="btn btn-info btn-lg" onclick="confirmDeleteModal('112')">Delete Data With Id 112</button><br>--}}
 
 
 
@@ -357,10 +338,14 @@ $data = \App\mymodel::all();
 
 
 
-<style>
-    .idbu{
-        padding-left: 20px;
-    }
-</style>
+    <style>
+        .idbu{
+            padding-left: 20px;
+        }
+        #delete{
+            background: #e71313;
+            color: whitesmoke;
+        }
+    </style>
 
 </div>
